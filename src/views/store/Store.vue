52590a2e0765cd330a43046b1076a62c431c9d28
Greg Bomkamp <bomkampg@gmail.com>
2020-09-16 17:43:41 -0400

    Adding missed files

---
+++ src/views/store/Store.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col class="py-0" cols="12">
+        <v-row class="py-0" justify="center" align="center">
+          <em>Coming Soon...</em>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="stylus"></style>
